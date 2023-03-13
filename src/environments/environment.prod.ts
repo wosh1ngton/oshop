d27b21a8442ebe4b07cf523356f726be79b790be
wosh1ngton <woshington.rod@gmail.com>
2023-03-13 08:23:08 -0300

    index

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: 'oshop-76ab3',
+    appId: '1:236205865924:web:4b621e53d317173b6a4291',
+    storageBucket: 'oshop-76ab3.appspot.com',
+    apiKey: 'AIzaSyDpQAc62zrP6IXUJbEF6t2MrNqkAtaLibw',
+    authDomain: 'oshop-76ab3.firebaseapp.com',
+    messagingSenderId: '236205865924',
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'oshop-76ab3',
+    appId: '1:236205865924:web:4b621e53d317173b6a4291',
+    storageBucket: 'oshop-76ab3.appspot.com',
+    apiKey: 'AIzaSyDpQAc62zrP6IXUJbEF6t2MrNqkAtaLibw',
+    authDomain: 'oshop-76ab3.firebaseapp.com',
+    messagingSenderId: '236205865924',
+  }
 };
 
 /*
